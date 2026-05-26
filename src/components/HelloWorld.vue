c4d987752a5f2e1547cb5bef16fb2d8435388136
Abdumajidov2005 <otkirbekabdumajidov28@gmail.com>
2026-05-26 13:47:25 +0500

    vuejsda gi loyihani yuklashni boshlayabman

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Salom Vue</h1>
+    <p>Mening birinchi pageim</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
