8441c4d494588980e312cd161fdc15fa2476bd67
Abdumajidov2005 <otkirbekabdumajidov28@gmail.com>
2026-06-12 15:16:39 +0500

    feature/header

---
+++ src/components/cart/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { dataBases } from "../data/app";
+</script>
+<template>
+  <div
+    v-for="item in dataBases"
+    :key="item.id"
+    class="w-80 shadow text-center py-6 px-8"
+  >
+    <component :is="item.svg" class="w-10 h-10 mx-auto" />
+    <h1 class="text-[#323232] text-[16px] lg:text-[28px] font-bold py-6">
+      {{ item.name }}
+    </h1>
+    <p class="text-[#323232] text-[12px] lg:text-[14px] font-normal">
+      {{ item.desc }}
+    </p>
+  </div>
+</template>
--- src/components/sections/Products.vue
+++ src/components/sections/Products.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { HomeIcon } from "@heroicons/vue/20/solid";
+import Card from "../cart/Card.vue";
 </script>
 <template>
   <section>
@@ -7,17 +7,8 @@ import { HomeIcon } from "@heroicons/vue/20/solid";
       <h1 class="text-[#323232] text-4xl font-bold">
         Saiba dicas essenciais para dar o conforto que o seu pet merece!
       </h1>
-      <div class="flex items-center justify-between flex-wrap">
-        <div class="w-80 shadow text-center py-6 px-8">
-          <HomeIcon class="w-10 h-10 mx-auto" />
-          <h1 class="text-[#323232] text-[16px] lg:text-[28px] font-bold py-6">
-            Adestramento
-          </h1>
-          <p class="text-[#323232] text-[12px] lg:text-[14px] font-normal">
-            Corrija comportamentos, eduque, divirta-se e fortaleça a relação com
-            seu pet.
-          </p>
-        </div>
+      <div class="flex items-center justify-between flex-wrap gap-10 py-6">
+        <Card />
       </div>
     </div>
   </section>
